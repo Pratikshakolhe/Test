6ad0f01d31e7792cd5a929e54808d4e3df8ac55c
Pratikshakolhe <78258593+Pratikshakolhe@users.noreply.github.com>
2021-02-17 02:06:07 -0800

    Add files via upload

---
+++ ArrayDemo.java
@@ -0,0 +1,40 @@
+package test1;
+import java.util.Scanner;
+
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		int children=0;
+		int adult=0;
+		int senior=0;
+		int age[]=new int[10];
+		Scanner sc=new Scanner(System.in);
+		
+		for(int i=0;i<age.length;i++)
+		{
+			age[i]=sc.nextInt();
+			
+		}
+		for(int i=0;i<age.length;i++)
+		{
+			if(age[i]<18)
+			{
+				children++;
+			}else if(age[i]>=18 && age[i]<=54)
+			{
+				adult++;
+				
+			}else if(age[i]>=55)
+			{
+				senior++;
+			}
+			
+		}
+		
+		System.out.println("children :"+children);
+		System.out.println("Adult :"+adult);
+		System.out.println("senior :"+senior);
+		}
+
+}
